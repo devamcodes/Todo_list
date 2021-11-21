b55b0da7c39e43ff7db63b79174a74fdf393a80c
Devam Agrawal <dev.devam.a@gmail.com>
2021-11-21 12:45:51 +0530

    New Router v6 Implemented

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,6 +2,7 @@
 Description: This is a simple todo list which gives you the functionality of adding new todo tasks, mark them as completed and remove the tasks. Total no of tasks are shown in the left drawer.
 status: Complete
 Pending: Add the functionality for editing the task, add validation in the title and the dates, add some more features like which task has the earliest end-date, total no. of tasks completed and the total no. of tasks remaining.
+You can also add functionality of clciking the todo which will open up a modal window which shows the details of that todo, after this you can also add the edit todo functionality.
 */
 
 import { useState } from "react";
